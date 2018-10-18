69ffc552ee0b6cd413f0d228ec8e3280eb7ef124
maly phongsavanh and Scott Bergler <scott@>
2018-10-18 11:43:34 -0700

    add functionality to multiple page elements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,38 @@
 $(document).ready(function() {
- // var age = parseInt(prompt("What is your age?"));
- //
- // if (age > 18) {
- //   $("#eligible").show();
- // } else if (age === 18){
- //   $("#congrats").show();
- // } else {
- //   $("#ineligible").show();
- // }
-$("form").submit(function(event) {
+  $(".userInput").submit(function(event) {
+    event.preventDefault();
+
+    var name = $("#userName").val();
+    var age = parseInt($("#ageField").val());
+    var party = $("#party").val();
+
+    if (name) {
+      $("#inputName").addClass("has-success");
+    } else {
+      $("#inputName").addClass("has-error");
+    }
+
+   if (age > 0) {
+     $("#inputAge").addClass("has-success");
+   } else {
+     $("#inputAge").addClass("has-error");
+   }
+   if(age >= 18) {
+     if (party === "democrat") {
+      $("#fred").show()
+     } else if (party === "republican") {
+       $("#toody").show();
+     } else if (party === "independent") {
+       $("#nugent").show();
+     }
+
+    } else {
+        $("#ineligible").show();
+    }
+
+});
+
+$(".candidates").submit(function(event) {
 
    var candidate = $("input:radio[name=candidate]:checked").val();
 
@@ -24,5 +48,6 @@ $("form").submit(function(event) {
 
   event.preventDefault();
  });
+
 
 });
